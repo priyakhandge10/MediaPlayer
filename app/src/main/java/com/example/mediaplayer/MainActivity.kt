2e88e85f92dd857c3fb13a73ee959f7980f0c484
priyanka khandge <priyakhandge10@gmail.com>
2020-04-11 13:04:48 +0530

    combine songs

--- app/src/main/java/com/example/mediaplayer/MainActivity.kt
+++ app/src/main/java/com/example/mediaplayer/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-      //  loadURLOnline()
+        loadURLOnline()
         CheckUserPermsions()
       //  adapter=MySongAdapter(listSongs)
        // lsListSongs.adapter=adapter
